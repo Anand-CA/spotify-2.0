9fff3454077139ed6020b6073710ce887ecf0c8d
densec <densec@densecs-iMac.local>
2021-07-03 12:03:41 +0530

    user authorization completed

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { getAccessToken, login_url } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getAccessToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  console.log("token", token);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      Let's build a spotify clone 2.0 🚀
+      <a
+        href={login_url}
+        style={{
+          backgroundColor: "#008CBA",
+          color: "#fff",
+          padding: "10px 15px",
+          cursor: "pointer",
+        }}
+      >
+        Login
+      </a>
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,25 @@
+export const client_secret = "718ede135a544ad781004897bf6d0e64";
+export const client_id = "3379b41787bf4903b57dbbf703f48016";
+export const redirect_uri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+export const getAccessToken = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const login_url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes.join(
+  "%20"
+)}&show_dialog=true`;
